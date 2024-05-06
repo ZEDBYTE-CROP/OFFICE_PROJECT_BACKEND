b2cb15ade24652d1159249309f06e5e885a2d0fe
ZEDBYTE <96447594+Zedbyte-corp@users.noreply.github.com>
2024-05-06 13:18:05 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 // cors options
 var corsOptions = {
-    origin: ['http://localhost:5173','http://localhost:5000', 'https://office-project-backend.onrender.com'],
+    origin: ['http://localhost:5173','http://localhost:5000', 'https://office-project-frontend.onrender.com'],
     credentials: true,
     optionsSuccessStatus: 200,
     exposedHeaders: ['CL-X-TOKEN','CL-X-REFRESH']
