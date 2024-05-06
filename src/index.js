bb53da3294fa5b891dad4b1c2d7bce2f4077b723
ZEDBYTE <96447594+Zedbyte-corp@users.noreply.github.com>
2024-05-06 13:27:32 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,6 +19,10 @@ var corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 //ROUTES (add as many)
 app.use("/account", account);
