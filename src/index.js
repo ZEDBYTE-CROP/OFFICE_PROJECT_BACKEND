7ef4ab0da1ca166c4893b7db5ff0760813612fdd
ZEDBYTE <96447594+Zedbyte-corp@users.noreply.github.com>
2024-05-06 13:35:14 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 // cors options
 var corsOptions = {
-    origin: ['http://localhost:5173','http://localhost:5000', 'https://office-project-frontend.onrender.com'],
+    origin: ['http://localhost:5173','http://localhost:5000', 'https://office-project-frontend.onrender.com','*'],
     credentials: true,
     optionsSuccessStatus: 200,
     exposedHeaders: ['CL-X-TOKEN','CL-X-REFRESH']
@@ -19,10 +19,7 @@ var corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  next();
-});
+
 
 //ROUTES (add as many)
 app.use("/account", account);
